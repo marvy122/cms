9f02b476baabdeeb12c0fadd74f7e5fd5edf2df1
marvy122 <146741518+marvy122@users.noreply.github.com>
2023-10-02 06:57:06 -0700

    Add files via upload

---
+++ cms/aboutus.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" type="text/css" href="aboutus.css">
+</head>
+<body>
+<div class="about-section">
+  <h1>About Us</h1>
+ <p>Canteen Managemenent System.</p>
+  <p>Crowd isnt always fun. We know that your time is valuable and sometimes every minute in the day counts. That’s why we deliver! So you can spend more time doing the things you love. You can get anything from our canteen food just by placing a simple order online through our web application. Then just sit back, relax, and wait for your order to arrive..</p>
+</div>
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="MARVY.jpg" alt="MARVY SHRESTHA" style="width:100%" >
+      <div class="container">
+          <h2>MARVY SHRESTHA </h2>
+        <p class="title">Developer</p>
+        <p>You can contact me through this mail.</p>
+        <p>marbsy6@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+<img src="milan.jpg" alt="MILAN KUMAR NIRAULA" style="width:90%">
+ <div class="container">
+ <h2>MILAN KUMAR NIRAULA </h2>
+<p class="title">Designer</p>
+ <p>You can contact me through this mail.</p>
+<p>gamerkurkure8@gmail.com</p>
+  <p><button class="button">Contact</button></p>
+    </div>
+    </div>
+  </div>
+    <div class= "log"> <a href="canteen.php">Go Home</a></div>
+</body>
+</html>
